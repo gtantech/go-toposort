61302de16ece2126a31fd2901ec7fabbabc34488
gtantech <tangabrielkl@gmail.com>
2026-08-26 07:52:00 -0400

    BREAKING: Changed Vertices to return iterator

--- graph/graph.go
+++ graph/graph.go
@@ -8,7 +8,7 @@ import (
 
 type Graph[V any, E any] interface {
 	OutgoingVertices(vertex vertex.Vertex[V]) func(yield func(vertex.Vertex[V]) bool)
-	Vertices() []vertex.Vertex[V]
+	Vertices() func(yield func(vertex.Vertex[V]) bool)
 	AddVertex(v vertex.Vertex[V])
 	AddEdge(value E, origin vertex.Vertex[V], destination vertex.Vertex[V])
 	GetEdgeValue(origin vertex.Vertex[V], destination vertex.Vertex[V]) (E, bool)
@@ -18,19 +18,17 @@ type Graph[V any, E any] interface {
 var _ Graph[string, string] = (*dag[string, string])(nil)
 
 type dag[V any, E any] struct {
-	vertices           []vertex.Vertex[V]
 	incomingToOutgoing map[vertex.Vertex[V]]map[vertex.Vertex[V]]E
 	uniqueVerticies    map[vertex.Vertex[V]]struct{}
 }
 
 func New[V any, E any]() Graph[V, E] {
-	return &dag[V, E]{vertices: []vertex.Vertex[V]{}, incomingToOutgoing: make(map[vertex.Vertex[V]]map[vertex.Vertex[V]]E), uniqueVerticies: make(map[vertex.Vertex[V]]struct{})}
+	return &dag[V, E]{incomingToOutgoing: make(map[vertex.Vertex[V]]map[vertex.Vertex[V]]E), uniqueVerticies: make(map[vertex.Vertex[V]]struct{})}
 }
 
 func (d *dag[V, E]) AddVertex(v vertex.Vertex[V]) {
 	if _, ok := d.uniqueVerticies[v]; !ok {
 		d.uniqueVerticies[v] = struct{}{}
-		d.vertices = append(d.vertices, v)
 	}
 }
 
@@ -69,11 +67,9 @@ func (d *dag[V, E]) AddEdge(value E, origin vertex.Vertex[V], destination vertex
 	}
 	if _, ok := d.uniqueVerticies[origin]; !ok {
 		d.uniqueVerticies[origin] = struct{}{}
-		d.vertices = append(d.vertices, origin)
 	}
 	if _, ok := d.uniqueVerticies[destination]; !ok {
 		d.uniqueVerticies[destination] = struct{}{}
-		d.vertices = append(d.vertices, destination)
 	}
 }
 
@@ -85,6 +81,6 @@ func (d *dag[V, E]) OutgoingVertices(v vertex.Vertex[V]) func(yield func(vertex.
 }
 
 // Vertices implements [graph.Graph].
-func (d *dag[V, E]) Vertices() []vertex.Vertex[V] {
-	return d.vertices
+func (d *dag[V, E]) Vertices() func(yield func(vertex.Vertex[V]) bool) {
+	return maps.Keys(d.uniqueVerticies)
 }
